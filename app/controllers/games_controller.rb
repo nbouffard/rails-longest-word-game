1ed69f8b747557cbf2ebb89c2b5832420af61552
nicola bouffard <nicola.bouffard855@gmail.com>
2023-05-12 16:08:26 +0200

    working game, need to add styling

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,25 @@
+require 'json'
+require 'open-uri'
+
 class GamesController < ApplicationController
   def new
-    @letters = ('a'..'z').to_a.sample(10)
+    @letters = ('A'..'Z').to_a.sample(10)
   end
 
   def score
+    @word = params[:word]
+    url = "https://wagon-dictionary.herokuapp.com/#{@word}"
+    word_serialized = URI.open(url).read
+    word = JSON.parse(word_serialized)
+    @letters = params[:letters].delete(' ').split('')
+    @word.chars.all? do |letter|
+      if @letters.include?(letter.upcase) && word['found'].to_s == 'true'
+        @result = "#{@word} is an english word"
+      elsif @letters.include?(letter.upcase) == false && word['found'].to_s == 'true'
+        @result = "#{@word} cannot be made using the letters of the grid"
+      else
+        @result = "#{@word} is not an english word"
+      end
+    end
   end
 end
